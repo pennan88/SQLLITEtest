ffb0af53cedaa7b11971061cb90db3b00622b185
Alexander Fagrelius <erikalexander98@hotmail.com>
2020-10-20 12:03:07 +0200

    tte

--- SQLLITE/Program.cs
+++ SQLLITE/Program.cs
@@ -7,7 +7,22 @@ namespace SQLLITE
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var connection = new SqliteConnection(@"DateSource=..\..\..\video_games_v02.db"))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = @"SELECT Title FROM video_games";
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while(reader.Read())
+                    {
+                        var title = reader.GetString(0);
+
+                        Console.WriteLine(@"Game name: {title}");
+                    }
+                }
+            }
         }
     }
 }
