598eb78f893444a1ad58b119011f8ed366872a76
Alexander Fagrelius <erikalexander98@hotmail.com>
2020-10-20 13:00:29 +0200

    La till lite extra till SELECT

--- SQLLITE/Program.cs
+++ SQLLITE/Program.cs
@@ -7,23 +7,29 @@ namespace SQLLITE
     {
         static void Main(string[] args)
         {
-            using (var connection = new SqliteConnection(@"Data Source = ..\..\..\ video_games_v02.db"))
+            using (var connection = new SqliteConnection(@"Data Source= ..\..\..\VideoGameList.db"))
             {
                 connection.Open();
                 var command = connection.CreateCommand();
                 command.CommandText =
                     @"
-                    SELECT Title 
+                    SELECT Title, Publishers, Year, Sales, Review_Score
                     FROM video_games
                     ";
 
                 using (var reader = command.ExecuteReader())
+
                 {
                     while (reader.Read())
                     {
                         var title = reader.GetString(0);
+                        var publishers = reader.GetString(1);
+                        var year = reader.GetString(2);
+                        var sales = reader.GetString(3);
+                        var score = reader.GetString(4);
 
-                        Console.WriteLine($"Game name: {title}");
+
+                        Console.WriteLine($"Game name: {title} \n Publishers: {publishers} \n Year: {year} \n Sales: {sales} Millions \n Score: {score}/100 ");
                     }
                 }
             }
