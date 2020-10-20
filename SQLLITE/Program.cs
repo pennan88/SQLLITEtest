b6cf6bc767df6f06eeab7578ce5d4989830f1d3e
Alexander Fagrelius <erikalexander98@hotmail.com>
2020-10-20 11:25:36 +0200

    Add project files.

---
+++ SQLLITE/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace SQLLITE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
